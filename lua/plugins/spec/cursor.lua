f8a6007f3da3eeea7a3dc719ba495d92a1165e49
FatngatirBilek <fathirbimashabri@gmail.com>
2025-04-25 11:33:03 +0700

    cursor trail fixed

--- lua/plugins/spec/cursor.lua
+++ lua/plugins/spec/cursor.lua
@@ -1,6 +1,7 @@
 ---@type NvPluginSpec
 return {
     "sphamba/smear-cursor.nvim",
+    lazy = false,
     opts = {
         -- Smear cursor when switching buffers or windows.
         smear_between_buffers = true,
