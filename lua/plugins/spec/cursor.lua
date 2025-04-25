ed4b02908b76895eb819177a67484f08c563f85a
FatngatirBilek <fathirbimashabri@gmail.com>
2025-04-25 11:13:53 +0700

    cursor trail

--- lua/plugins/spec/cursor.lua
+++ lua/plugins/spec/cursor.lua
@@ -1,56 +1,23 @@
 ---@type NvPluginSpec
 return {
-	{
-		"saecki/crates.nvim",
-		event = { "BufRead Cargo.toml" },
-		tag = "stable",
-		opts = function(_, opts)
-			local crates = require("crates")
+    "sphamba/smear-cursor.nvim",
+    opts = {
+        -- Smear cursor when switching buffers or windows.
+        smear_between_buffers = true,
 
-			vim.keymap.set("n", "<leader>cu", function()
-				crates.upgrade_all_crates()
-			end, { desc = "Update crates" })
+        -- Smear cursor when moving within line or to neighbor lines.
+        -- Use `min_horizontal_distance_smear` and `min_vertical_distance_smear` for finer control
+        smear_between_neighbor_lines = true,
 
-			local options = {
-				completion = {
-					cmp = {
-						enabled = true,
-					},
-				},
-			}
+        -- Draw the smear in buffer space instead of screen space when scrolling
+        scroll_buffer_space = true,
 
-			opts = vim.tbl_deep_extend("force", opts or {}, options)
-			return opts
-		end,
-	},
+        -- Set to `true` if your font supports legacy computing symbols (block unicode symbols).
+        -- Smears will blend better on all backgrounds.
+        legacy_computing_symbols_support = false,
 
-	-- Smear cursor plugin with default options
-	{
-		"sphamba/smear-cursor.nvim",
-		opts = {},
-	},
-
-	-- Smear cursor plugin with additional options
-	{
-		"sphamba/smear-cursor.nvim",
-		opts = {
-			-- Smear cursor when switching buffers or windows.
-			smear_between_buffers = true,
-
-			-- Smear cursor when moving within line or to neighbor lines.
-			-- Use `min_horizontal_distance_smear` and `min_vertical_distance_smear` for finer control
-			smear_between_neighbor_lines = true,
-
-			-- Draw the smear in buffer space instead of screen space when scrolling
-			scroll_buffer_space = true,
-
-			-- Set to `true` if your font supports legacy computing symbols (block unicode symbols).
-			-- Smears will blend better on all backgrounds.
-			legacy_computing_symbols_support = false,
-
-			-- Smear cursor in insert mode.
-			-- See also `vertical_bar_cursor_insert_mode` and `distance_stop_animating_vertical_bar`.
-			smear_insert_mode = true,
-		},
-	},
+        -- Smear cursor in insert mode.
+        -- See also `vertical_bar_cursor_insert_mode` and `distance_stop_animating_vertical_bar`.
+        smear_insert_mode = true,
+    },
 }
--- lua/plugins/spec/supermaven.lua
+++ lua/plugins/spec/supermaven.lua
@@ -1,14 +1,14 @@
 return {
-  "supermaven-inc/supermaven-nvim",
-  event = "LspAttach",
-  opts = {
-    keymaps = {
-      accept_suggestion = "<Tab>",
-      clear_suggestion = "<C-x>",
-      accept_word = "<C-y>",
+    "supermaven-inc/supermaven-nvim",
+    event = "LspAttach",
+    opts = {
+        keymaps = {
+            accept_suggestion = "<Tab>",
+            clear_suggestion = "<C-x>",
+            accept_word = "<C-y>",
+        },
+        disable_keymaps = false,
+        log_level = "warn",
+        disable_inline_completion = false,
     },
-    disable_keymaps = false,
-    log_level = "warn",
-    dsable_inline_completion = false,
-  },
 }
